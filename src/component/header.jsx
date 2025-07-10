5bf77060be63f3a92f58d72e90421f463f80f8d8
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-10 14:06:49 +0530

    add: implement responsive header with navigation and cart icon

---
+++ src/component/header.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
+export default function Header() {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="w-pull h-[75px]  flex justify-start bg-gray-200 relative items-center">
+        <GiHamburgerMenu className="lg:hidden text-3xl text-accent mx-4" onClick={()=>{
+          setOpen(!isOpen);
+        }}/>
+        <div className="hidden lg:flex w-[400px] h-full justify-evenly items-center text-red-900 text-xl">
+          <Link to="/">Home</Link>
+          <Link to="/products">Product</Link>
+          <Link to="/contact-us">Contact Us</Link>
+          <Link to="/overview">Overview</Link>
+          <Link to="/cart" className="absolute right-[30px] text-3xl">
+            <ShoppingCart />
+          </Link>
+        </div>
+        {isOpen && (
+          <div className="fixed lg:hidden z-[9999] top-0 left-0 bg-[#00000060] w-full h-screen flex">
+            <div className="w-[300px] h-full bg-white  flex flex-col justify-start items-start p-4">
+              <GiHamburgerMenu className="text-3xl text-accent" onClick={()=>{
+                setOpen(!isOpen);
+              }}/>
+              <Link to="/">Home</Link>
+              <Link to="/products">Product</Link>
+              <Link to="/contact-us">Contact Us</Link>
+              <Link to="/overview">Overview</Link>
+              <Link to="/cart" className="absolute right-[30px] text-3xl">
+                <ShoppingCart />
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
