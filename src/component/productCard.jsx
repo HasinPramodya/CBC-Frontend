03f7a4d7da144bc97c81338c557344ad7479ab3c
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-10 14:11:05 +0530

    add: implement ProductCard component for product display

---
+++ src/component/productCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function ProductCard(props){
+      
+ 
+
+    return(
+        <>
+          <Link to={"/overview/"+props.product.productId} className="w-[250px] h-[350px] m-4 bg-amber-400 border">
+             <img  className="w-full h-[200px] object-cover" src={props.product.images[0]}/>
+             <div className="h-[110px] w-full flex flex-col justify-center items-center">
+               <span className="text-lg font-bold">{props.product.name}</span>
+               <p>{props.product.price}</p>
+               <p>{props.product.description}</p>
+
+             </div>
+          </Link>
+        </>
+    )
+}
