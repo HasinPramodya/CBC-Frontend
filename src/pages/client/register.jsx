683f6bfadeb64a430d059235b7478f95e908f32e
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-11 20:10:37 +0530

    feature: make the register page mobile responsive

--- src/pages/client/register.jsx
+++ src/pages/client/register.jsx
@@ -37,48 +37,47 @@ export default function RegisterPage() {
 
   return (
     <>
-      <div className="w-full h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center flex">
-        <div className="w-[50%] h-full border-[1px]"></div>
-        <div className="w-[50%] h-full border-[1px] flex justify-center items-center">
-          <div className="w-[550px] h-[700px] backdrop-blur-xl shadow-xl rounded-b-xl flex flex-col justify-center items-center">
+      <div className="w-full h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center flex justify-center items-center">
+        <div className="w-full h-full  flex justify-center items-center">
+          <div className="w-full h-full lg:w-[600px] lg:h-[700px] backdrop-blur-xl shadow-xl rounded-b-xl flex flex-col justify-center items-center">
             <input
-              className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[4px]"
+              className="w-full h-12 lg:w-[400px] border border-white rounded-xl text-center m-[4px]"
               type="email"
               placeholder="Email"
               onChange={(e) => setEmail(e.target.value)}
             />
             <input
-              className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[4px]"
+              className="w-full h-12 lg:w-[400px]  border border-white rounded-xl text-center m-[4px]"
               type="text"
               placeholder="First Name"
               onChange={(e) => setFirstName(e.target.value)}
             />
             <input
-              className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[4px]"
+              className="w-full h-12 lg:w-[400px]  border border-white rounded-xl text-center m-[4px]"
               type="text"
               placeholder="Last Name"
               onChange={(e) => setLastName(e.target.value)}
             />
             <input
-              className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[4px]"
+              className="w-full h-12 lg:w-[400px]  border border-white rounded-xl text-center m-[4px]"
               type="text"
               placeholder="Role"
               onChange={(e) => setRole(e.target.value)}
             />
             <input
-              className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[4px]"
+              className="w-full h-12 lg:w-[400px]  border border-white rounded-xl text-center m-[4px]"
               type="password"
               placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
             />
             <input
-              className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[4px]"
+              className="w-full h-12 lg:w-[400px]  border border-white rounded-xl text-center m-[4px]"
               type="text"
               placeholder="Phone"
               onChange={(e) => setPhone(e.target.value)}
             />
             <button
-              className="w-[400px] h-[50px] border border-white bg-green-500 rounded-xl text-center text-white cursor-pointer mt-[6px]"
+              className="w-full h-12 lg:w-[400px] border border-white bg-green-500 rounded-xl text-center text-white cursor-pointer mt-[6px]"
               onClick={handleRegister}
             >
               Register
