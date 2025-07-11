783c90d54c5f13696dd6132c88e1cd99c6b2d428
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-11 20:08:47 +0530

    feature: add slider for the home page

---
+++ src/component/homeSlider.jsx
@@ -0,0 +1,39 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Slider from "react-slick";
+export default function HomeSlider() {
+
+       const images = [
+           "./public/home-slider-1.jpg",
+           "./public/home-slider-2.jpg",
+           "./public/home-slider-3.jpg",
+        ];
+
+        const settings = {
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 2000,
+            autoplaySpeed: 2000,
+            cssEase: "linear"
+        };
+
+  return (
+    <>
+        <div className="slider-container">
+            <Slider {...settings}>
+                {
+                    images.map((image, index) => (
+                        <div key={index} className="slider-item">
+                            <img src={image} alt={`Slide ${index}`} className="w-full h-full object-cover" />
+                        </div>
+                    ))
+                }
+            </Slider>
+        </div>
+    </>
+  )
+}
