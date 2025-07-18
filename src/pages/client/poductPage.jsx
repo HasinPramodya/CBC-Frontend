2e8ba3577d2094bddb9e81602b8dafd43f180280
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-18 11:41:47 +0530

    feature: add search product function

--- src/pages/client/poductPage.jsx
+++ src/pages/client/poductPage.jsx
@@ -4,8 +4,9 @@ import Loader from "../../component/loader";
 import ProductCard from "../../component/productCard";
 
 export default function ProductPage() {
-  const [productList, setProducrtList] = useState([]);
+  const [productList, setProductList] = useState([]);
   const [prodictsLoaded, setproductsLoaded] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     if (!prodictsLoaded) {
@@ -17,7 +18,7 @@ export default function ProductPage() {
         })
         .then((res) => {
           // console.log(res.data)
-          setProducrtList(res.data.products);
+          setProductList(res.data.products);
           setproductsLoaded(true);
         })
         .catch((error) => {
@@ -26,8 +27,36 @@ export default function ProductPage() {
     }
   }, [prodictsLoaded]);
 
+  function searchProducts(){
+      axios
+          .get(import.meta.env.VITE_BACKEND_URL + "/api/product/search/" + search, {
+              headers: {
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+          })
+          .then((res) => {
+              console.log(res.data.products);
+              setProductList(res.data.products);
+          })
+          .catch((err) => {
+              console.log(err);
+          });
+
+  }
+
   return (
     <>
+      <div className="w-full h-[50px] flex items-center justify-center">
+        <input type="text" placeholder="Search" value={search} className="w-[300px] h-[30px] border-2 border-gray-300 rounded-md p-2" onChange={(e)=>{setSearch(e.target.value)}}/>
+        <button className="bg-blue-500 text-white p-2 rounded-md ml-2" onClick={()=>{
+          searchProducts()
+        }}>Search</button>
+          <button className="bg-blue-500 text-white p-2 rounded-md ml-2" onClick={()=>{
+              setproductsLoaded(false)
+
+          }
+          }>Reset</button>
+      </div>
       <div className="w-full h-screen">
         {prodictsLoaded ? 
           <div className="w-full h-full flex flex-wrap justify-center item-center">
