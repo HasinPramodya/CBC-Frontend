ae96b62345bba2b79f8382785183a435637b1370
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-20 00:29:22 +0530

    feature: add cart to mobile side bar

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -4,13 +4,13 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 import UserData from "./userData";
 export default function Header() {
-  const [isOpen, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       <div className="w-pull h-[75px]  flex justify-start bg-gray-200 relative items-center">
         <GiHamburgerMenu className="lg:hidden text-3xl text-accent mx-4" onClick={()=>{
-          setOpen(!isOpen);
+          setIsOpen(!isOpen);
         }}/>
         <div className="hidden lg:flex w-[400px] h-full justify-evenly items-center text-red-900 text-xl">
           <Link to="/">Home</Link>
@@ -20,7 +20,7 @@ export default function Header() {
           <Link to="/cart" className="absolute right-[30px] text-3xl">
             <ShoppingCart />
           </Link>
-          <div className="absolute right-[20px] h-full flex">
+          <div className="absolute right-[70px] h-full flex">
             <UserData />
           </div>
         </div>
@@ -28,15 +28,18 @@ export default function Header() {
           <div className="fixed lg:hidden z-[9999] top-0 left-0 bg-[#00000060] w-full h-screen flex">
             <div className="w-[300px] h-full bg-white  flex flex-col justify-start items-start p-4">
               <GiHamburgerMenu className="text-3xl text-accent" onClick={()=>{
-                setOpen(!isOpen);
+                setIsOpen(!isOpen);
               }}/>
               <Link to="/">Home</Link>
               <Link to="/products">Product</Link>
               <Link to="/contact-us">Contact Us</Link>
               <Link to="/about-us">About Us</Link>
-              <Link to="/cart" className="absolute right-[30px] text-3xl">
+              <Link to="/cart" className="text-3xl mb-2">
                 <ShoppingCart />
               </Link>
+              <div>
+                <UserData />
+              </div>
             </div>
           </div>
         )}
