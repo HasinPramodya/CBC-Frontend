4705b574511bafa920042577fa5dfe3b0299099b
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-22 01:39:47 +0530

    fix: correct mismatch imports

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../component/loader.jsx";
-import ImageSlider from "../../component/imageslider.jsx";
+import ImageSlider from "../../component/imageSlider.jsx";
 import { addToCart } from "../../utills/cart";
 import toast from "react-hot-toast";
 
