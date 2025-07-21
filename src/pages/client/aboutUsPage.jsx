131df36ab161d694a1f11c9323cff5e099179b36
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-22 01:31:06 +0530

    feature: add AboutUsPage component

---
+++ src/pages/client/aboutUsPage.jsx
@@ -0,0 +1,48 @@
+export default function AboutUsPage() {
+    return (
+        <>
+            <div className="w-full h-full rounded-lg flex justify-center items-center">
+                <div className="w-full h-full lg:w-[800px] lg:h-[850px] rounded-lg bg-white shadow-lg flex flex-col p-[40px] items-center">
+                    <h1 className="text-4xl font-bold">About Us</h1>
+                    <div>
+                        <h1 className="text-lg mt-4 font-bold">Our Story</h1>
+                        <p className="text-md mt-4">
+                            COSMETICS.LK was initiated in the year 2018 as a sister company of the LMJ International Lanka. LMJ is the exclusive sole agent for PALMER’S premium personal care from the USA and WILD FERNS natural personal care products from New Zealand.
+                        </p>
+                        <p className="text-md mt-4">
+                            While marketing our products through the trade over the years we noticed that some or the most of the customers were having a difficulty in finding the required products on the shelf at the real time and then we had the urge of serving the customers directly to avoid further disappointments, that is where COSMETICS.LK was launched.
+                        </p>
+                        <p className="text-md mt-4">
+                            We strive to become the largest online cosmetics store in Sri Lanka, offering a wide range of beauty and healthcare products, including a growing selection of vegan, cruelty-free, and non-toxic items. Our mission is to deliver a unique and world-class online shopping experience to every customer.
+                        </p>
+
+                        <h1 className="text-lg mt-4 font-bold">Our Vision</h1>
+                        <p className="text-md mt-4">
+                            “To be the market leader in providing an easy access for Beauty, Wellness & Personal Care products and personalized services.”
+                        </p>
+
+                        <h1 className="text-lg mt-4 font-bold">Our Mission</h1>
+
+                            <ul className="list-disc pl-5">
+                                <li>Enhance beauty and confidence in our clients by giving superior products and services.</li>
+                                <li>Build and Maintain a strong & true relationship with our employees, suppliers and most importantly with customers in terms of marketing & distribution.</li>
+                                <li>Provide an excellent customer service through a skilled & professional sales and marketing team</li>
+                            </ul>
+
+
+
+                        <h1 className="text-lg mt-4 font-bold">Our Values</h1>
+                        <ul className="list-disc pl-5">
+                            <li>INTERGRITY</li>
+                            <li>PROFESSIONALISM</li>
+                            <li>CARING</li>
+                            <li>CARING</li>
+                            <li>CARING</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
