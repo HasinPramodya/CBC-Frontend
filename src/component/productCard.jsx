b3968b49d4c0b67f6c58f2c2bbbb7ac25b02a0e1
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-21 16:30:30 +0530

    fix: optimize the product card component

--- src/component/productCard.jsx
+++ src/component/productCard.jsx
@@ -6,9 +6,9 @@ export default function ProductCard(props){
 
     return(
         <>
-          <Link to={"/overview/"+props.product.productId} className="w-[250px] h-[350px] m-4 bg-amber-400 border">
+          <Link to={"/overview/"+props.product.productId} className="w-[250px] h-[400px] m-4 bg-amber-400 border">
              <img  className="w-full h-[200px] object-cover" src={props.product.images[0]}/>
-             <div className="h-[110px] w-full flex flex-col justify-center items-center">
+             <div className="h-[200px] w-full flex flex-col justify-center items-center">
                <span className="text-lg font-bold">{props.product.name}</span>
                <p>{props.product.price}</p>
                <p>{props.product.description}</p>
