102195bf12ebc762494881d84d53aeb777abee12
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-16 11:24:52 +0530

    Add Supabase media upload utility for handling file uploads and retrieving public URLs.

--- src/utills/mediaUplaod.jsx
+++ src/utills/mediaUplaod.jsx
@@ -1,32 +1,37 @@
-import {createClient} from "@supabase/supabase-js"
-import toast from "react-hot-toast"
-const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL,import.meta.env.VITE_SUPABASE_ANON_KEY)
-export default function MediaUpload(file){
-    
-    return new Promise(
-        (reslove,reject)=>{
-          
-            if(file==null){
-                toast.error("Please select a file")
-            }else{
-                const timpStamp = new Date().getTime();
-                const fileName = timpStamp+file.name;
+import { createClient } from "@supabase/supabase-js";
+import toast from "react-hot-toast";
 
-                supabaseClient.storage.from("images").upload(fileName,file).then(
-                    ()=>{
-                       const url = supabaseClient.storage.from("images").getPublicUrl(fileName).data.publicUrl
-                       reslove(url)
-                    }
-                ).catch(
-                    (error)=>{
-                        console.log(error)
-                        reject(error)
-                    }
-                )
-            }
-        }
-    )
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+const supabaseClient = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+export default function MediaUpload(file) {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      toast.error("Please select a file");
+      reject("No file selected");
+      return;
+    }
+
+    const timeStamp = Date.now();
+    const fileName = timeStamp + file.name;
+
+    supabaseClient.storage
+      .from("images")
+      .upload(fileName, file)
+      .then(() => {
+        const url = supabaseClient.storage
+          .from("images")
+          .getPublicUrl(fileName).data.publicUrl;
+        resolve(url);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
 }
-
-
-  
